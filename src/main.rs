2959fc068b2d6ff4d1934f3b520e37325e21bb30
ben <ben@home.com>
2022-12-03 09:34:07 +1100

    day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let contents = fs::read_to_string("day1.txt")?;
+    
+    let cals = contents.split("\n");
+    let mut elves = vec![];
+    let mut curr = 0;
+
+    for x in cals {
+        if x.is_empty() {
+            elves.push(curr);
+            curr = 0;
+        }
+        else {
+            curr += x.parse::<i32>()?;
+        }
+    }
+
+    match elves.iter().enumerate().max_by(|(_, a), (_, b)| a.cmp(b)) {
+        None => println!("No MAX"),
+        Some((i, x)) => println!("MAX: {}/{}", i, x)
+    }
+    Ok(())
 }
